eb81abbf6dcd279e9884533a75d1062ba73d3357
furkan <furkanhald@gmail.com>
2022-01-16 13:02:27 +0300

    first

--- SehirSimge/ViewController.swift
+++ SehirSimge/ViewController.swift
@@ -1,19 +1,44 @@
 //
 //  ViewController.swift
-//  SehirSimge
+//  SehirSimgeleri
 //
 //  Created by Furkan halidi on 16.01.2022.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    
+    var landmarkNames = [String]()
+    var landmarkImages = [UIImage]()
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+      
+        landmarkNames.append("Colosseum")
+        landmarkNames.append("Great Wall")
+        landmarkNames.append("Kremlin")
+        landmarkNames.append("Taj Mahal")
+       
+        landmarkImages.append(UIImage(named: "colossium.jpg")!)
+        landmarkImages.append(UIImage(named: "greatwall.jpg")!)
+        landmarkImages.append(UIImage(named: "kremlin.jpg")!)
+        landmarkImages.append(UIImage(named: "tajmahal.jpg")!)
+        
     }
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = landmarkNames[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return landmarkNames.count
+    }
 }
 
